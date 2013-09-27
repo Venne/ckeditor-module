f8ca63803a1c1c3814d43d13866401611a8c6677
Josef Kříž <pepakriz@gmail.com>
2013-09-27 20:20:24 +0200

    Updated CKEditor to 4.2.1

--- Resources/public/editor.js
+++ Resources/public/editor.js
@@ -6,31 +6,46 @@ $(function () {
 		var basePath = "";
 	}
 
-	var config = {
-		filebrowserBrowseUrl:basePath + '/admin/en/files?lang=cs&panel-tab=2&browserMode=1',
-		filebrowserImageBrowseUrl:basePath + '/admin/en/files?lang=cs&panel-tab=2&browserMode=1',
-		filebrowserWindowWidth:'1024',
-		filebrowserWindowHeight:'768',
-		height:550
-	};
-
 	function initCkeditor() {
 		$('textarea[venne-form-editor]').each(function () {
 			if (!$(this).data('ckeditor')) {
 				$(this).data('ckeditor', true);
 				$(this).parent().prev().hide();
 				$(this).parent().css('margin-left', 0);
-				CKEDITOR.replace($(this).attr('id'), config);
+
+				var _this = $(this);
+
+				var config = {
+					filebrowserBrowseUrl: basePath + '/admin/en/files?lang=cs&panel-tab=2&browserMode=1',
+					filebrowserImageBrowseUrl: basePath + '/admin/en/files?lang=cs&panel-tab=2&browserMode=1',
+					filebrowserWindowWidth: '1024',
+					filebrowserWindowHeight: '768',
+					height: 550
+				};
+
+				$.ajax({
+					url: basePath + '/public/ckeditor/backend.json',
+					dataType: "json",
+					success: function () {
+						var c = jQuery.parseJSON('{ "name": "John" }');
+					}
+				})
+					.done(function (data) {
+						config = $.extend({}, config, data);
+					})
+					.always(function () {
+						CKEDITOR.replace(_this.attr('id'), config);
+					});
 			}
 		});
 
 	}
 
 	$.nette.ext('ckeditor', {
-		load: function() {
+		load: function () {
 			initCkeditor();
 		},
-		complete:function (payload) {
+		complete: function (payload) {
 			initCkeditor();
 		}
 	});
